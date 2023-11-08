2c60a818bcd400c075a8ff028fe24921b8fbd2e5
moonknight324 <141109828+moonknight324@users.noreply.github.com>
2023-11-08 17:09:45 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,109 @@
+let imageVirender = document.getElementById('virender-img');
+let virender = document.getElementById('virender');
+virender.addEventListener('click', function () {
+  imageVirender.style.display = 'block';
+  imageVirender.style.transform = 'translate(5em, -20em)';
+  imageVirender.onmouseleave = () => {
+    imageVirender.style.display = 'none';
+  };
+});
+
+let imageGambhir = document.getElementById('gambhir-img');
+let gambhir = document.getElementById('gautam');
+gambhir.addEventListener('click', function () {
+  imageGambhir.style.display = 'block';
+  imageGambhir.style.transform = 'translate(6em, -18em)';
+  imageGambhir.onmouseleave = () => {
+    imageGambhir.style.display = 'none';
+  };
+});
+
+let imageSachin = document.getElementById('sachin-img');
+let sachin = document.getElementById('sachin');
+sachin.addEventListener('click', function () {
+  imageSachin.style.display = 'block';
+  imageSachin.style.transform = 'translate(6em, -16em)';
+  imageSachin.onmouseleave = () => {
+    imageSachin.style.display = 'none';
+  };
+});
+
+let imageVirat = document.getElementById('virat-img');
+let virat = document.getElementById('virat');
+virat.addEventListener('click', function () {
+  imageVirat.style.display = 'block';
+  imageVirat.style.transform = 'translate(4em, -14em)';
+  imageVirat.onmouseleave = () => {
+    imageVirat.style.display = 'none';
+  };
+});
+
+let imageDhoni = document.getElementById('dhoni-img');
+let dhoni = document.getElementById('ms');
+dhoni.addEventListener('click', function () {
+  imageDhoni.style.display = 'block';
+  imageDhoni.style.transform = 'translate(6em, -19em)';
+  imageDhoni.onmouseleave = () => {
+    imageDhoni.style.display = 'none';
+  };
+});
+
+let imageYuvraj = document.getElementById('yuvraj-img');
+let yuvraj = document.getElementById('yuvraj');
+yuvraj.addEventListener('click', function () {
+  imageYuvraj.style.display = 'block';
+  imageYuvraj.style.transform = 'translate(6em, -12em)';
+  imageYuvraj.onmouseleave = () => {
+    imageYuvraj.style.display = 'none';
+  };
+});
+
+let imageSuresh = document.getElementById('suresh-img');
+let suresh = document.getElementById('suresh');
+suresh.addEventListener('click', function () {
+  imageSuresh.style.display = 'block';
+  imageSuresh.style.transform = 'translate(10em, 0em)';
+  imageSuresh.onmouseleave = () => {
+    imageSuresh.style.display = 'none';
+  };
+});
+
+let imageHarbhajan = document.getElementById('harbhajan-img');
+let harbhajan = document.getElementById('harbhajan');
+harbhajan.addEventListener('click', function () {
+  imageHarbhajan.style.display = 'block';
+  imageHarbhajan.style.transform = 'translate(20em, 0em)';
+  imageHarbhajan.onmouseleave = () => {
+    imageHarbhajan.style.display = 'none';
+  };
+});
+
+let imageZaheer = document.getElementById('zaheer-img');
+let zaheer = document.getElementById('zaheer');
+zaheer.addEventListener('click', function () {
+  imageZaheer.style.display = 'block';
+  imageZaheer.style.transform = 'translate(27em, 0em)';
+  imageZaheer.onmouseleave = () => {
+    imageZaheer.style.display = 'none';
+  };
+});
+
+let imageMunaf = document.getElementById('munaf-img');
+let munaf = document.getElementById('munaf');
+munaf.addEventListener('click', function () {
+  imageMunaf.style.display = 'block';
+  imageMunaf.style.transform = 'translate(35em, 4em)';
+  imageMunaf.onmouseleave = () => {
+    imageMunaf.style.display = 'none';
+  };
+});
+
+let imageSreesanth = document.getElementById('sreesanth-img');
+let sreesanth = document.getElementById('sreesanth');
+sreesanth.addEventListener('click', function () {
+  imageSreesanth.style.display = 'block';
+  imageSreesanth.style.transform = 'translate(37em, 3em)';
+  imageSreesanth.onmouseleave = () => {
+    imageSreesanth.style.display = 'none';
+  };
+});
